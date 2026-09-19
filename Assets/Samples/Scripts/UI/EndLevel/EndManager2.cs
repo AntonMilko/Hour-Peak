7886d68f9363d25eafe294bb1821b8d92331c87b
AntonMilko <antonmilko2@gmail.com>
2026-09-19 20:02:53 +0300

    Кое-какие правки в EndManager2

--- Assets/Samples/Scripts/UI/EndLevel/EndManager2.cs
+++ Assets/Samples/Scripts/UI/EndLevel/EndManager2.cs
@@ -7,16 +7,9 @@ public class EndManager2 : MonoBehaviour
 {
     [Header("UI References")]
     public GameObject endMenuPanel;
-    
-    // --- ГРУППА УСПЕХА ---
     public Button nextButton; 
     public Button againButton; 
     public Button exitButton; 
-
-    // --- ГРУППА НЕУДАЧИ ---
-    public Button nextFailedButton; 
-    public Button againFailedButton; 
-    public Button exitFailedButton; 
 
     [Header("Time Evaluation")]
     public TextMeshProUGUI timeText;
@@ -40,8 +33,6 @@ public class EndManager2 : MonoBehaviour
     public void ShowFinalSuccessScreen()
     {
         Debug.Log("[END] ФИНАЛ ПРОЙДЕН!");
-        
-        HideAllButtons();
 
         if (nextButton != null)
         {
@@ -55,34 +46,6 @@ public class EndManager2 : MonoBehaviour
 
         if (timeText != null) 
             timeText.text = "Время: Отлично! Уровень пройден.";
-
-        if (endMenuPanel != null) 
-            endMenuPanel.SetActive(true);
-    }
-
-    /// <summary>
-    /// ВЫЗЫВАТЬ ПРИ ПРОИГРЫШЕ НА ФИНАЛЬНОМ УРОВНЕ.
-    /// ПРАВИЛО: Горят ТОЛЬКО againFailedButton и exitFailedButton.
-    /// </summary>
-    public void ShowFailureScreen()
-    {
-        Debug.Log("[END] УРОВЕНЬ НЕ ПРОЙДЕН.");
-
-        HideAllButtons();
-
-        if (againFailedButton != null)
-        {
-            againFailedButton.gameObject.SetActive(true);
-            againFailedButton.onClick.RemoveAllListeners();
-            againFailedButton.onClick.AddListener(RestartLevel);
-        }
-
-        if (exitFailedButton != null)
-        {
-            exitFailedButton.gameObject.SetActive(true);
-            exitFailedButton.onClick.RemoveAllListeners();
-            exitFailedButton.onClick.AddListener(ExitToMenu);
-        }
 
         if (endMenuPanel != null) 
             endMenuPanel.SetActive(true);
@@ -114,15 +77,5 @@ public class EndManager2 : MonoBehaviour
         {
             SceneManager.LoadScene(scenePartMainMenu);
         }
-    }
-
-    private void HideAllButtons()
-    {
-        if (nextButton != null) nextButton.gameObject.SetActive(false);
-        if (nextFailedButton != null) nextFailedButton.gameObject.SetActive(false); 
-        if (againButton != null) againButton.gameObject.SetActive(false);
-        if (againFailedButton != null) againFailedButton.gameObject.SetActive(false);
-        if (exitButton != null) exitButton.gameObject.SetActive(false);
-        if (exitFailedButton != null) exitFailedButton.gameObject.SetActive(false);
     }
 }
